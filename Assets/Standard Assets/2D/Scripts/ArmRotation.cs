a17cdfb0b01bf0dbb00551b03faf79a9dac0f5cd
junok <u0830932@utah.edu>
2016-09-29 20:21:20 -0600

    added the project

---
+++ Assets/Standard Assets/2D/Scripts/ArmRotation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArmRotation : MonoBehaviour {
+
+
+    public int rotation_offset = 90;
+	// Update is called once per frame
+	void Update () {
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        difference.Normalize();
+
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + rotation_offset);
+	}
+}
---
+++ Assets/Standard Assets/2D/Scripts/Weapon.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour {
+
+    public float fireRate = 0;
+    public float Damage = 10;
+    public LayerMask notToHit;
+    public GameObject Bullet;
+    [Tooltip("How fast the bullet will fire")]
+    public float bulletSpeed = 5;
+    public float bulletLifeTime = 3;
+
+
+    private float timeToFire = 0;
+    private Transform firePoint;
+
+	// Use this for initialization
+	void Awake () {
+        firePoint = transform.FindChild("FirePoint");
+        if (firePoint == null)
+        {
+            Debug.LogError("no firepoint");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (fireRate == 0)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Shoot();
+            }
+        }
+        else
+        {
+            if (Input.GetButton("Fire1") && Time.time > timeToFire)
+            {
+                timeToFire = Time.time + 1/fireRate;
+                Shoot();
+            }
+        }
+
+	}
+
+    void Shoot()
+    {
+        Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        Vector2 firePointPosition = new Vector2(firePoint.position.x, firePoint.position.y);
+        RaycastHit2D hit = Physics2D.Raycast(firePointPosition, mousePosition-firePointPosition, 100, notToHit);
+
+
+        GameObject tempBulletHandler;
+        tempBulletHandler = Instantiate(Bullet, firePoint.position, transform.rotation) as GameObject;
+        Rigidbody2D tempRigidBody;
+        tempRigidBody = tempBulletHandler.GetComponent<Rigidbody2D>();
+        Debug.Log(tempBulletHandler.transform.up);
+        tempRigidBody.AddForce(tempBulletHandler.transform.up.normalized * bulletSpeed, ForceMode2D.Impulse);
+
+        Destroy(tempBulletHandler, bulletLifeTime);
+    }
+}
