491cd4ac4d267c8996da15dbc30eb03f4d661a90
junok <u0830932@utah.edu>
2016-10-05 21:43:23 -0600

    Got Explosion working

--- Assets/Standard Assets/2D/Scripts/BulletExplosion.cs
+++ Assets/Standard Assets/2D/Scripts/BulletExplosion.cs
@@ -4,6 +4,10 @@ using System.Collections;
 public class BulletExplosion : MonoBehaviour {
 
     public float knockBackStunTime = 5;
+    public float knockBackSpeed = 50;
+
+    private float xdirection = 1;
+    private float ydirection = 1;
 	// Use this for initialization
 	void Start () {
 	
@@ -18,12 +22,28 @@ public class BulletExplosion : MonoBehaviour {
     {
         if (trigger.gameObject.tag == "Player")
         {
+            if (trigger.gameObject.transform.position.x <= this.transform.position.x)
+            {
+                xdirection = -1;
+            } 
+            else
+            {
+                xdirection = 1;
+            }
+            if (trigger.gameObject.transform.position.y <= this.transform.position.y)
+            {
+                ydirection = -1;
+            }
+            else
+            {
+                ydirection = 1;
+            }
             Debug.Log("Hit");
-            trigger.gameObject.GetComponent<UnityStandardAssets._2D.Platformer2DUserControl>().enabled = false;
+            //trigger.gameObject.GetComponent<UnityStandardAssets._2D.Platformer2DUserControl>().enabled = false;
             
-            trigger.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(-1000, 0, 0);
+            trigger.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(xdirection,ydirection,0).normalized * knockBackSpeed;
             StartCoroutine(knockBackStun(knockBackStunTime));
-            trigger.gameObject.GetComponent<UnityStandardAssets._2D.Platformer2DUserControl>().enabled = true;      
+            //trigger.gameObject.GetComponent<UnityStandardAssets._2D.Platformer2DUserControl>().enabled = true;      
         }
     }
 
