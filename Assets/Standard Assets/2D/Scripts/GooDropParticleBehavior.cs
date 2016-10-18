b1709c4b3479f134307a687a6e6a26643438af3a
kelmer77 <kalebelmer@hotmail.com>
2016-10-17 19:27:04 -0600

    Finished up the projectile explosion
    Added fragments and animated explosion sprite that occur when the
    projectile is destroyed.

---
+++ Assets/Standard Assets/2D/Scripts/GooDropParticleBehavior.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GooDropParticleBehavior : MonoBehaviour 
+{
+	private float timer;
+	public float minDeathTime;
+	public float maxDeathTime;
+
+	void Start()
+	{
+		timer = Random.Range (minDeathTime, maxDeathTime);
+	}
+
+	void Update()
+	{
+		if (timer > 0)
+			timer--;
+		else
+			Destroy (gameObject);
+	}
+}
---
+++ Assets/Standard Assets/2D/Scripts/ParticleDemo.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleDemo : MonoBehaviour 
+{
+	public GameObject projectileParticle;
+    public GameObject projectileExplosion;
+	public float minSpeed;
+	public float maxSpeed;
+
+	void OnDestroy()
+	{
+        GameObject explosion = Instantiate (projectileExplosion, gameObject.transform.position, Quaternion.identity) as GameObject;
+
+        int randCount = Random.Range (4, 8);
+
+		for (int i = 0; i < randCount; i++)
+		{
+			GameObject particle = Instantiate (projectileParticle, gameObject.transform.position, Quaternion.identity) as GameObject;
+
+			//create random direction
+			Vector2 randDir = new Vector2 (Random.Range (-10, 10), Random.Range (-10, 10));
+			randDir.Normalize ();
+
+			//create random speed
+			float randSpeed = Random.Range (minSpeed, maxSpeed);
+
+			particle.GetComponent<Rigidbody2D> ().AddForce (randDir * randSpeed);
+		}
+
+	}
+}
